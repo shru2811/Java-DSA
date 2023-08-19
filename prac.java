f8f834a9743996ede2d633906e062e9a9ca7bc93
Shruti Srivastava <113620272+shru2811@users.noreply.github.com>
2023-08-19 21:55:59 +0530

    empty file will contain algorithm programs

--- prac.java
+++ prac.java
@@ -1,17 +1,9 @@
 
                                     /*THIS FILE WILL CONTAIN THE ALGORITHM QUESTIONS*/
 
-
-import com.sun.security.jgss.GSSUtil;
-
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 public class prac {
     public static void main(String[] args) {
-        int [] num = {2,1,5};
-        int k = 806;
-        ArrayList<Integer> num2 = new ArrayList<Integer>();
+       
     }
 }
