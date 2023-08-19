1c5798a71e8381af994b092f3495a321fdefc906
Shruti Srivastava <sshruti.2811@gmail.com>
2023-08-19 19:10:52 +0530

    updated

--- demo2.java
+++ demo2.java
@@ -1,4 +1,15 @@
+public class Stack{
+    int[] arr;
+    int top;
 
+    public Stack(int size){
+        this.arr = new int[size];
+        this.top = -1;
+        System.out.println("the stack is created with the size of: "+size);
+    }
+}
 
 public class demo2 {
+    Stack st = new Stack(5);
+
 }
