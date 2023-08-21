bdbae2b6af11145f91f6823fbc08bec79941a4a6
Shruti Srivastava <sshruti.2811@gmail.com>
2023-08-21 22:15:03 +0530

    added insertion sort

--- prac.java
+++ prac.java
@@ -1,17 +1,23 @@
 
                                     /*THIS FILE WILL CONTAIN THE ALGORITHM QUESTIONS*/
 
-
-import com.sun.security.jgss.GSSUtil;
-
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 public class prac {
+    public static void insSort(int[] arr){
+        for(int i=1;i<arr.length;i++){              //itterate through the unsorted array
+            int j = i-1;                            //
+            int key = arr[i];                       //store the unsorted element in this key
+            while(j>=0 && arr[j]>key){              //iterate through the sorted array to slide elements to the right so that we can create space for insertion
+                arr[j+1]=arr[j];
+                j--;                                //where loop terminates we insert element at its place
+            }
+            arr[j+1]=key;
+        }
+    }
     public static void main(String[] args) {
-        int [] num = {2,1,5};
-        int k = 806;
-        ArrayList<Integer> num2 = new ArrayList<Integer>();
+        int[] arr =  {5,4,3,2,1};
+        insSort(arr);
+        for(int i:arr){
+            System.out.println(i);
+        }
     }
 }
